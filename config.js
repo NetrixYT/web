861f47437f316828d27c0c56f83b40845ec47609
Netrix YT <44086362+NetrixYT@users.noreply.github.com>
2024-06-23 20:42:36 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  //Port
+  PORT: process.env.PORT || 5000,
+
+  //Secret key for API
+  SECRET_KEY: "8VVCdCq8lf",
+
+  //Gmail credentials for send email
+  EMAIL: "xxnovachargexx@gmail.com",
+  PASSWORD: "orxjutjqsmrqvqaw",
+
+  //Secret key for jwt
+  JWT_SECRET: "ECJv9JJqy0",
+
+  //baseURL
+  baseURL: "https://mozyminer.com/shop2/",
+
+  //Firebase server key for send notification through firebase
+  SERVER_KEY: "BFyWndk3HNtxzC1QRzqX7XYQ_MIaNXATo5wez_7CW8Vklw1XdmkBKoW3mZNrHzB4Qben7iES60lrTwNRgokFLv4",
+};
